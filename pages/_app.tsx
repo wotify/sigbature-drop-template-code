b37ba6959f8f7dc157a2b65faeb163f54cfefecb
igor <igor_batinic@yahoo.com>
2023-01-25 13:53:59 +0100

    Add in _app.jsx import next/head + Head component with title element.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,19 @@ import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import ThirdwebGuideFooter from "../components/GitHubLink";
+import Head from "next/head";
 
+// added import next/head and Head component with title element
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Goerli;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Wotify Minting DApp</title>
+      </Head>
       <Component {...pageProps} />
       <ThirdwebGuideFooter />
     </ThirdwebProvider>
